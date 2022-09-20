9dad359f68dc9ce088c1af7ed2342199ea152cf8
attilajak <41372370+attilajak@users.noreply.github.com>
2022-09-20 22:31:59 +0530

    first commit

---
+++ podrunner.py
@@ -0,0 +1,10 @@
+class PodRunner():
+
+    def __init__(self,event,app):
+        pass
+
+    def ProcessEvent(self,event,app):
+        pass
+
+    def GetServiceStatus(self):
+        return self.service_status
