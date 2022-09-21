84c9edb808e7a3673f5fd32e25cc03aef3962ced
attilajak <41372370+attilajak@users.noreply.github.com>
2022-09-21 14:01:54 +0530

    first commit

--- nfhealthcheck.py
+++ nfhealthcheck.py
@@ -8,6 +8,7 @@ import signal
 import json
 import urllib3
 import logging
+from podrunner import PodRunner
 from watchevent import WatchEvent
 
 
@@ -26,12 +27,7 @@ service_mode = "production"
 #service_mode = "standalone"
 
 
-pod_dictionary = {
-  "podname": {
-    "app" : "applabel"
-  }
- 
-}
+pod_dictionary = {}
 
 handler = logging.StreamHandler()
 log_format = "%(asctime)s [%(levelname)s] [%(module)s] [%(funcName)s] %(message)s"
@@ -49,7 +45,7 @@ config.load_incluster_config()
 
 v1 = client.CoreV1Api()
 
-def process_udrstatus(udr_status):
+def process_servicestatus(status,app):
 
     log.info("Enter")
 
@@ -68,141 +64,44 @@ def process_udrstatus(udr_status):
         e = sys.exc_info()[0]
         log.info("file errors",e)
 
-    udr_nf_instance = targetnflists["udrnflist"][0]
-    udr_profile = nfprofiles[udr_nf_instance]
-
-    udsf_nf_instance = targetnflists["udsfnflist"][0]
-    udsf_profile = nfprofiles[udsf_nf_instance]
+    if app == "udr":
+        nf_instance = targetnflists["udrnflist"][0]
+        profile = nfprofiles[nf_instance]
+    elif app == "udsf":
+        nf_instance = targetnflists["udsfnflist"][0]
+        profile = nfprofiles[nf_instance]
     
     http = urllib3.PoolManager()
 
-    if udr_status == 0:
-        udr_profile["nfStatus"] = "SUSPENDED"
-        try:
-            log.info('Connecting to:'+ 'https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/suspend/'+udr_nf_instance)
-            con = http.request('PUT','https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/suspend/'+udr_nf_instance,body=json.dumps(udr_profile))
-            log.info("Exit")
-            if con.status == 200:
-                log.info("Connection Success")
-            else:
-                log.info(con.status)
-                log.info(con.headers)
-        except KeyboardInterrupt:
-            log.info("Keyboard Interrupt Exception")
-            log.info("Exit")
-        except:
-            e = sys.exc_info()[0]
-            log.info("Exception",e)
-            log.info("Exit")
-            raise RuntimeError
-        
+    if not status:
+        profile["nfStatus"] = "SUSPENDED"
+        url = 'https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/suspend/'+nf_instance
+    elif status:
+        profile["nfStatus"] = "REGISTERED"
+        url = 'https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/register/'+nf_instance
 
-    elif udr_status == 1:
-        udr_profile["nfStatus"] = "REGISTERED"
-        try:
-            log.info('Connecting to:'+ 'https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/register/'+udr_nf_instance)
-            con = http.request('PUT','https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/register/'+udr_nf_instance,body=json.dumps(udr_profile))
-            if con.status == 200:
-                log.info("Connection Success")
-            else:
-                log.info(con.status)
-                log.info(con.headers)
-            log.info("Exit")
-        except KeyboardInterrupt:
-            log.info("Keyboard Interrupt Exception")
-            log.info("Exit")
-        except:
-            e = sys.exc_info()[0]
-            log.info("Connection Error %s",e)
-            log.info("Exit")
-            raise RuntimeError
+    try:
+        log.info('Connecting to:'+ url)
+        con = http.request('PUT',url,body=json.dumps(profile))
+        log.info("Exit")
+        if con.status == 200:
+            log.info("Connection Success")
+        else:
+            log.info(con.status)
+            log.info(con.headers)
+    except KeyboardInterrupt:
+        log.info("Keyboard Interrupt Exception")
+        log.info("Exit")
+    except:
+        e = sys.exc_info()[0]
+        log.info("Exception",e)
+        log.info("Exit")
+        raise RuntimeError
         
         
-        
-def process_udsfstatus(udsf_status):
-    log.info("Enter")
-
-    targetnflistfile = open("targetnflist.json", "r")
-
-    targetnflists = json.loads(targetnflistfile.read())
-
-    targetnflistfile.close()
-
-    nfprofilesfile = open("nfprofiles.json", "r")
-
-    nfprofiles = json.loads(nfprofilesfile.read())
-
-    nfprofilesfile.close()
-
-    nrfcaddressfile = open("config.json", "r")
-    nrfcaddress = json.loads(nrfcaddressfile.read())
-    service_host = nrfcaddress[service_mode][0]["url"]
-
-    nrfcaddressfile.close()
-
-    udr_nf_instance = targetnflists["udrnflist"][0]
-    udr_profile = nfprofiles[udr_nf_instance]
-
-    udsf_nf_instance = targetnflists["udsfnflist"][0]
-    udsf_profile = nfprofiles[udsf_nf_instance]
-    
-    http = urllib3.PoolManager()
-   
-
-    if udsf_status == 0:
-        # read config and send SUSPENDED to NRFC
-        udsf_profile["nfStatus"] = "SUSPENDED"
-        try:
-            log.info('Connecting to:'+ 'https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/register/'+udsf_nf_instance)
-            con = http.request('PUT','https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/suspend/'+udsf_nf_instance,body=json.dumps(udsf_profile))
-            if con.status == 200:
-                log.info("Connection Success")
-            else:
-                log.info(con.status)
-                log.info(con.headers)
-            log.info("Exit")
-        except KeyboardInterrupt:
-            log.info("Connection Error - Keyboard Interrupt ")
-            log.info("Exit")
-        except:
-            e = sys.exc_info()[0]
-            log.info("Connection Error %s",e)
-            log.info("Exit")
-            raise RuntimeError
-        
-    elif udsf_status == 1:
-        # read config and send REGISTERED to NRFC
-        udsf_profile["nfStatus"] = "REGISTERED"
-        try:
-            log.info('Connecting to:'+ 'https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/register/'+udsf_nf_instance)
-            con = http.request('PUT','https://'+service_host+":"+ str(nrfcaddress[service_mode][0]["port"])+'/nrfclient/register/'+udsf_nf_instance,body=json.dumps(udsf_profile))
-            if con.status == 200:
-                log.info("Connection Success")
-            else:
-                log.info(con.status)
-                log.info(con.headers)
-            log.info("Exit")
-        except KeyboardInterrupt:
-            log.info("Connection Error - Keyboard Interrupt ")
-            log.info("Exit")
-        except:
-            e = sys.exc_info()[0]
-            log.info("Connection Error %s",e)
-            log.info("Exit")
-            raise RuntimeError
-
-
-
-
-
 async def pods():
 
-    udr_running_count = 0
-    udsf_running_count = 0
     nrfc_running = 0
-
-    udr_status = 0
-    udsf_status = 0
 
     log.info("Enter")
     w = watch.Watch()
@@ -228,63 +127,30 @@ async def pods():
                     pod_dictionary.pop(watchevent.pod_name)
                     nrfc_running = 0
 
+            if watchevent.pod_applabel == "udr":
+                runner,statuschange = PodRunner(watchevent,"udr")
+            elif watchevent.pod_applabel == "udsf":
+                runner,statuschange = PodRunner(watchevent,"udsf")
 
-            #check for number of running udr pods
-            if watchevent.pod_name not in pod_dictionary and watchevent.pod_applabel == "udr" and watchevent.isPodReady() == True:
-                pod_dictionary[watchevent.pod_name] = { "app" : watchevent.pod_applabel }
-                udr_running_count += 1
-
-            if watchevent.pod_name not in pod_dictionary and watchevent.pod_applabel == "udsf" and watchevent.isPodReady() == True:
-                pod_dictionary[watchevent.pod_name] = { "app" : watchevent.pod_applabel }
-                udsf_running_count += 1
-
-
-            if watchevent.isPodReady() == False and watchevent.pod_applabel == "udr":
-                if watchevent.pod_name in pod_dictionary:
-                    pod_dictionary.pop(watchevent.pod_name)
-                udr_running_count -= 1
-            if watchevent.isPodReady() == False and watchevent.pod_applabel == "udsf":
-                if watchevent.pod_name in pod_dictionary:
-                    pod_dictionary.pop(watchevent.pod_name)
-                udsf_running_count -= 1
+            
             if watchevent.isPodReady() == False and watchevent.pod_applabel == "nrfc":
                 if watchevent.pod_name in pod_dictionary:
                     pod_dictionary.pop(watchevent.pod_name)
                     nrfc_running = 0
-
-            log.info("udr_running_count: %s, udsf_running_count: %s" % (udr_running_count,udsf_running_count))
-
-            if udr_status == 0 and udr_running_count >= 2:
-                if nrfc_running == 1:
-                    udr_status = 1
-                    try:
-                        process_udrstatus(udr_status)
-                    except ( RuntimeError) as e:
-                        log.info("process_udrstatus error %s",e)
-            elif udr_status == 1 and udr_running_count < 2:
-                  if nrfc_running == 1:
-                      udr_status = 0
-                      try:
-                          process_udrstatus(udr_status)
-                      except ( RuntimeError) as e:
-                          log.info("process_udrstatus error %s",e)
             
-            if udsf_status == 0 and udsf_running_count >= 2:
+            if watchevent.pod_applabel == "udr" and statuschange:
                 if nrfc_running == 1:
-                    udsf_status = 1
                     try:
-                        process_udsfstatus(udsf_status)
+                        process_servicestatus(runner.GetServiceStatus(),"udr")
                     except ( RuntimeError) as e:
-                        log.info("process_udsfstatus error %s",e)
-            elif udsf_status == 1 and udsf_running_count < 2:
-                  if nrfc_running == 1:
-                      udsf_status = 0
-                      try:
-                          process_udsfstatus(udsf_status)
-                      except ( RuntimeError) as e:
-                          log.info("process_udsfstatus error %s",e)
+                        log.info("process_servicestatus error %s",e)
+            elif watchevent.pod_applabel == "udsf" and statuschange:
+                if nrfc_running == 1:
+                    try:
+                        process_servicestatus(runner.GetServiceStatus(),"udsf")
+                    except ( RuntimeError) as e:
+                        log.info("process_servicestatus error %s",e)
             
-            log.info("udr_status: %s, udsf_status: %s" % (udr_status,udsf_status))
             await asyncio.sleep(0)
 
     except KeyboardInterrupt:
--- podrunner.py
+++ podrunner.py
@@ -1,10 +1,31 @@
 class PodRunner():
 
+    running_pods = {}
+
+    service_status = False
+
+
     def __init__(self,event,app):
-        pass
+        return self.ProcessEvent(event,app)
 
     def ProcessEvent(self,event,app):
-        pass
+        retval = False
+        if event.pod_name not in self.running_pods and event.pod_applabel == app and event.isPodReady() == True:
+            self.running_pods[event.pod_name] = { "app" : event.pod_applabel }
+        elif event.pod_name in self.running_pods and event.pod_applabel == app and event.isPodReady() != True:
+            self.running_pods.pop(event.pod_name)
+        else:
+            return retval
+        if len(self.running_pods) >= 2:
+            if not self.service_status:
+                retval = True
+            self.service_status = True
+        else:
+            if not self.service_status:
+                retval = True
+            self.service_status = False
+        return retval
+   
 
     def GetServiceStatus(self):
         return self.service_status
